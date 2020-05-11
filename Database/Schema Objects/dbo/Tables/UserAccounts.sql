ee09fecaad2a439f8268a4b65303d07087105b7f
Geoffrey Blogref <gmblogref@hotmail.com>
2020-05-11 11:31:49 -0500

    Adding logging table and stored proc

--- Database/Schema Objects/dbo/Tables/UserAccounts.sql
+++ Database/Schema Objects/dbo/Tables/UserAccounts.sql
@@ -1,17 +1,17 @@
 ﻿CREATE TABLE [dbo].[UserAccounts]
 (
-	[UserAccountsId]    INT             IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
-    [UserName]          VARCHAR(50)     CONSTRAINT [DF_UserAccounts_UserName]       DEFAULT ('') NOT NULL,
-    [FirstName]         VARCHAR(150)    NULL, 
-    [LastName]          VARCHAR(150)    NULL, 
-    [Email]             VARCHAR(50)     CONSTRAINT [DF_UserAccounts_Email]          DEFAULT ('') NOT NULL, 
-    [Address]           VARCHAR(50)     CONSTRAINT [DF_UserAccounts_Address]        DEFAULT ('') NOT NULL, 
-    [City]              VARCHAR(50)     CONSTRAINT [DF_UserAccounts_City]           DEFAULT ('') NOT NULL, 
-    [State]             VARCHAR(2)      CONSTRAINT [DF_UserAccounts_State]          DEFAULT ('') NOT NULL, 
-    [Zip]               VARCHAR(10)     CONSTRAINT [DF_UserAccounts_Zip]            DEFAULT ('') NOT NULL, 
-    [CreatedOn]         DATETIME        CONSTRAINT [DF_UserAccounts_CreatedOn]      DEFAULT (getdate()) NOT NULL, 
-    [LastUpdated]       DATETIME        CONSTRAINT [DF_UserAccounts_LastUpdated]    DEFAULT (getdate()) NOT NULL,
-    CONSTRAINT [PK_UserAccounts] PRIMARY KEY CLUSTERED ([UserAccountsId] ASC)
+	 [UserAccountsId]    INT             IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL
+    ,[UserName]          VARCHAR(50)     CONSTRAINT [DF_UserAccounts_UserName]       DEFAULT ('') NOT NULL
+    ,[FirstName]         VARCHAR(150)    NULL 
+    ,[LastName]          VARCHAR(150)    NULL 
+    ,[Email]             VARCHAR(50)     CONSTRAINT [DF_UserAccounts_Email]          DEFAULT ('') NOT NULL 
+    ,[Address]           VARCHAR(50)     CONSTRAINT [DF_UserAccounts_Address]        DEFAULT ('') NOT NULL 
+    ,[City]              VARCHAR(50)     CONSTRAINT [DF_UserAccounts_City]           DEFAULT ('') NOT NULL 
+    ,[State]             VARCHAR(2)      CONSTRAINT [DF_UserAccounts_State]          DEFAULT ('') NOT NULL 
+    ,[Zip]               VARCHAR(10)     CONSTRAINT [DF_UserAccounts_Zip]            DEFAULT ('') NOT NULL 
+    ,[CreatedOn]         DATETIME        CONSTRAINT [DF_UserAccounts_CreatedOn]      DEFAULT (getdate()) NOT NULL
+    ,[LastUpdated]       DATETIME        CONSTRAINT [DF_UserAccounts_LastUpdated]    DEFAULT (getdate()) NOT NULL
+    ,CONSTRAINT [PK_UserAccounts] PRIMARY KEY CLUSTERED ([UserAccountsId] ASC)
 );
 
 
