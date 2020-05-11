1979614ed06587d30db40966ada40a973a027f8e
Geoffrey Blogref <gmblogref@hotmail.com>
2020-05-11 10:33:18 -0500

    Adding rest of CRUD SPs for useraccounts table

---
+++ Database/Schema Objects/dbo/Stored Procedures/UserAccounts_Update.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[UserAccounts_Update]
+(
+	 @UserAccountsId    INT         
+	,@UserName          VARCHAR(50) 
+	,@FirstName         VARCHAR(150)
+	,@LastName          VARCHAR(150)
+	,@Email             VARCHAR(50) 
+	,@Address           VARCHAR(50) 
+	,@City              VARCHAR(50) 
+	,@State             VARCHAR(2)  
+	,@Zip               VARCHAR(10)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE
+		[dbo].[UserAccounts]
+	SET
+		 [UserName]		= @UserName
+		,[FirstName]	= @FirstName
+		,[LastName]		= @LastName
+		,[Email]		= @Email
+		,[Address]		= @Address
+		,[City]			= @City
+		,[State]		= @State
+		,[Zip]			= @Zip
+		,[LastUpdated]	= GETDATE()
+	WHERE
+		UserAccountsId = @UserAccountsId
+END
