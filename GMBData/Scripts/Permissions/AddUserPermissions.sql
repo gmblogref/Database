e09c499ed8055ab7bff8fe50b36a0a93b1d389a5
Geoffrey Blogref <gmblogref@hotmail.com>
2020-06-05 09:34:30 -0500

    adding post deploy scripts
    updated framework to 4.8
    
    changed assembly and namespace for GMBData to GMBData

---
+++ GMBData/Scripts/Permissions/AddUserPermissions.sql
@@ -0,0 +1,37 @@
+﻿--------------------------------------------------------------------------------
+-- Dot Net API User Setup
+PRINT 'Setting $(ApiNetUser) Permissions...';
+GO
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Set up Login in Master
+--------------------------------------------------------------------------------
+IF NOT EXISTS
+(   SELECT 1 FROM master.sys.server_principals p WHERE p.name = N'$(ApiNetUser)'
+)
+BEGIN
+    CREATE LOGIN [$(ApiNetUser)]
+        WITH PASSWORD=N'$(ApiNetPassword)', DEFAULT_DATABASE = [master], DEFAULT_LANGUAGE = [us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF;
+END
+
+--------------------------------------------------------------------------------
+-- Set up User in [$(DatabaseName)]
+--------------------------------------------------------------------------------
+IF NOT EXISTS
+(   SELECT 1 FROM sys.database_principals p WHERE p.name = N'$(ApiNetUser)'
+)
+BEGIN
+    CREATE USER [$(ApiNetUser)] FOR LOGIN [$(ApiNetUser)];
+END
+
+--------------------------------------------------------------------------------
+-- Add User to Roles
+--------------------------------------------------------------------------------
+ALTER ROLE [db_datareader]  ADD MEMBER [$(ApiNetUser)];
+ALTER ROLE [db_datawriter]  ADD MEMBER [$(ApiNetUser)];
+ALTER ROLE [db_execproc]    ADD MEMBER [$(ApiNetUser)];
+
+GRANT CONNECT SQL TO [$(ApiNetUser)]
+GO
+GRANT VIEW ANY DEFINITION TO [$(ApiNetUser)]
+GO
---
+++ GMBData/Scripts/SqlJobs/AddCustomDefinedRoles.sql
@@ -0,0 +1,26 @@
+﻿/*					
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ This file creates custom ROLES for the database 				
+--------------------------------------------------------------------------------------
+*/
+
+--------------------------------------------------------------------------------------
+/*
+	Adding db_execproc role
+	Will give users added to this role EXECUTE PERMS on all stored procedures
+	that are part of the [$(DatabaseSchema)]
+
+	https://www.patrickkeisler.com/2012/10/grant-execute-permission-on-all-stored.html
+*/
+--------------------------------------------------------------------------------------
+IF NOT EXISTS
+(   SELECT * FROM sys.database_principals p WHERE p.name = [$(ExecProcRole)]
+)
+BEGIN
+    CREATE ROLE [$(ExecProcRole)];
+END
+GO
+
+GRANT EXECUTE ON SCHEMA::[$(DatabaseSchema)] TO [$(ExecProcRole)];
+GO
